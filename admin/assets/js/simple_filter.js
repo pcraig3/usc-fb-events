ab5ebdacca9da2c627e4d158b9b4f23b1c055f11
Paul Craig <pcraig3@uwo.ca>
2014-06-10 04:05:45 -0400

    FINALLY pulling in and sorting what I asked for.

--- admin/assets/js/simple_filter.js
+++ admin/assets/js/simple_filter.js
@@ -1,75 +1,40 @@
 var fJS;
 
-var services = [
-    {
-        "permalink": "1-mr-ona-howe",
-        "title": "Mr. Ona Howe",
-        "amount": 100,
-        "status": "active",
-        "is_public": true,
-        "id": 1,
-        "nonprofit": "Eldon Zulauf"
-    },
-    {
-        "permalink": "2-darien-hoeger",
-        "title": "Darien Hoeger",
-        "amount": 201,
-        "id": 2,
-        "status": "inactive",
-        "nonprofit": "Beryl McDermott"
-    },
-    {
-        "permalink": "3-mrs-frederique-kris",
-        "title": "Mrs. Frederique Kris",
-        "amount": 500,
-        "id": 3,
-        "status": "active",
-        "nonprofit": "Eldon Zulauf"
-    },
-    {
-        "permalink": "4-jedediah-pouros",
-        "title": "Jedediah Pouros",
-        "amount": 300,
-        "id": 4,
-        "status": "inactive",
-        "nonprofit": "Mabel Tillman I"
-    },
-    {
-        "permalink": "5-mrs-daisy-macejkovic",
-        "title": "Mrs. Daisy Macejkovic",
-        "amount": 410,
-        "id": 5,
-        "status": "active",
-        "nonprofit": "Mr. Ayden O'Keefe"
-    } ];
-
-
 jQuery(document).ready(function($) {
 
   $('#status :checkbox').prop('checked', true);
 
-  fJS = filterInit();
+  //@TODO: This sucks
+  $.get( "http://testwestern.com/api/events/events/2014-04-01", function( data ) {
+
+      var data = data.events;
+      $.each(data, function(i, e){ e.id = i+1; });
+
+      fJS = filterInit(data);
+
+  }, "json" );
+
 });
 
-function filterInit() {
+function filterInit(data) {
 
-  var view = function(services){
+  var view = function(data){
         return "<div class='row'>" +
-            "<span class='name'>" + services.title + "</span>" +
-            "<span class='age'>" + services.amount + "</span>" +
-            "<span class='country'>" + services.status + "</span>" +
+            "<span class='name'>" + data.name + "</span>" +
+            "<span class='host'>" + data.host + "</span>" +
+            //"<span class='status'>" + data.status + "</span>" +
             "</div>";
   }
 
   var settings = {
     filter_criteria: {
       //amount: ['#price_filter .TYPE.range', 'amount'],
-      status: ['#status :checkbox', 'status']
+      //status: ['#status :checkbox', 'status']
     },
     search: {input: '#search_box' },
     and_filter_on: true,
     id_field: 'id' //Default is id. This is only for usecase
   };
 
-  return FilterJS(services, "#service_list", view, settings);
+  return FilterJS(data, "#service_list", view, settings);
 }
