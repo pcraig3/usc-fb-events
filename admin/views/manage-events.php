524ff503bbf81e035ce4b6e5c6096d5d073f55ff
Paul Craig <pcraig3@uwo.ca>
2014-06-14 23:18:20 -0400

    Just starting to (re)build out my second form.

--- admin/views/manage-events.php
+++ admin/views/manage-events.php
@@ -23,41 +23,68 @@ class Manage_Events extends AdminPageFramework {
             )
         );
 
-        /*$this->addSettingSections(
+        $this->addSettingSections(
             'manage_events_page',
             array(
-                'section_id' => 'first_section',
-                'title' => 'List Events',
-                'description' => 'List a bunch of Facebook events, plz.',
-            ),
-            array(
-                'section_id' => 'second_section',
-                'title' => 'Mod Events',
-                'description' => 'This section is for losers.',
+                'section_id' => 'modify_section',
+                'title' => 'Modify Events',
+                'description' => 'Change events or return them to their defaults.',
             )
         );
-        */
-        
+
         $this->addSettingFields(
-            //'first_section',
-            array(
-                'field_id'		=> 'checkbox_field',
-                'type'			=> 'checkbox',
-                'title'			=> 'What Checkbox Input',
-                'description'	=> __( 'The description for the apple.', 'admin-page-framework-demo' ),
-                'label'			=> __( 'This is a check box.', 'admin-page-framework-demo' ),
-            ),
-            array(
-                'field_id'		=> 'select_filed',
-                'type'			=> 'select',
-                'title'			=> __( 'Select Box', 'admin-page-framework-demo' ),
-                'description'	=> __( 'The description for the field.', 'admin-page-framework-demo' ),
-                'label' => array(
-                    'one' => __( 'One', 'admin-page-framework-demo' ),
-                    'two' => __( 'Two', 'admin-page-framework-demo' ),
-                    'three' => __( 'Three', 'admin-page-framework-demo' ),
+            'modify_section',
+            array(	// Single text field
+                'field_id'	=>	'text',
+                // 'section_id'	=>	'text_fields',	// can be omitted as it is set previously
+                'title'	=>	__( 'Text', 'admin-page-framework-demo' ),
+                'description'	=>	__( 'Type something here. This text is inserted with the <code>description</code> key in the field definition array.', 'admin-page-framework-demo' ),
+                'help'	=>	__( 'This is a text field and typed text will be saved. This text is inserted with the <code>help</code> key in the field definition array.', 'admin-page-framework-demo' ),
+                'type'	=>	'text',
+                'order'	=>	1,	// ( optional )
+                'default'	=>	123456,
+                'attributes'	=>	array(
+                    'size'	=>	40,
                 ),
-                'default' 			=> 'one',	// 0 means the first item
+            ),
+            array(	// Read-only
+                'field_id'	=>	'read_only_text',
+                'title'	=>	__( 'Read Only', 'admin-page-framework-demo' ),
+                'type'	=>	'text',
+                'attributes'	=>	array(
+                    'size'	=>	20,
+                    'readonly'	=>	'ReadOnly',
+                    // 'disabled'	=>	'Disabled',		// disabled can be specified like so
+                ),
+                'value'	=>	__( 'This is a read-only value.', 'admin-page-framework-demo' ),
+                'description'	=>	__( 'The attribute can be set with the <code>attributes</code> key.', 'admin-page-framework-demo' ),
+            ),
+            array(	// Multiple text fields
+                'field_id'	=>	'text_multiple',
+                'title'	=>	__( 'Multiple Text Fields', 'admin-page-framework-demo' ),
+                'help'	=>	__( 'Multiple text fields can be passed by setting an array to the label key.', 'admin-page-framework-demo' ),
+                'type'	=>	'text',
+                'default'	=>	'Hello World',
+                'label'	=>	'First Item: ',
+                'attributes'	=>	array(
+                    'size'	=>	20,
+                ),
+                'delimiter'	=>	'<br />',
+                array(
+                    'default'	=>	'Foo bar',
+                    'label'	=>	'Second Item: ',
+                    'attributes'	=>	array(
+                        'size'	=>	40,
+                    )
+                ),
+                array(
+                    'default'	=>	'Yes, we can',
+                    'label'	=>	'Third Item: ',
+                    'attributes'	=>	array(
+                        'size'	=>	60,
+                    )
+                ),
+                'description'	=>	__( 'These are multiple text fields. To include multiple input fields associated with one field ID, use the numeric keys in the field definition array.', 'admin-page-framework-demo' ),
             ),
             array( // Submit button
                 'field_id' => 'submit_button_b',
