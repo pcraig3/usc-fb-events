ef07d6ec81c233b5573159c3911f48fd7088e9d5
Paul Craig <pcraig3@uwo.ca>
2014-06-15 23:58:47 -0400

    Working again.

--- admin/views/manage-events.php
+++ admin/views/manage-events.php
@@ -34,57 +34,39 @@ class Manage_Events extends AdminPageFramework {
 
         $this->addSettingFields(
             'modify_section',
-            array(	// Single text field
-                'field_id'	=>	'text',
-                // 'section_id'	=>	'text_fields',	// can be omitted as it is set previously
-                'title'	=>	__( 'Text', 'admin-page-framework-demo' ),
-                'description'	=>	__( 'Type something here. This text is inserted with the <code>description</code> key in the field definition array.', 'admin-page-framework-demo' ),
-                'help'	=>	__( 'This is a text field and typed text will be saved. This text is inserted with the <code>help</code> key in the field definition array.', 'admin-page-framework-demo' ),
-                'type'	=>	'text',
-                'order'	=>	1,	// ( optional )
-                'default'	=>	123456,
-                'attributes'	=>	array(
-                    'size'	=>	40,
-                ),
-            ),
             array(	// Read-only
-                'field_id'	=>	'read_only_text',
-                'title'	=>	__( 'Read Only', 'admin-page-framework-demo' ),
+                'field_id'	=>	'test_event_name',
+                'title'	=>	'Event Name',
                 'type'	=>	'text',
+                'default'	=>	'Placeholder text',
                 'attributes'	=>	array(
-                    'size'	=>	20,
                     'readonly'	=>	'ReadOnly',
                     // 'disabled'	=>	'Disabled',		// disabled can be specified like so
                 ),
-                'value'	=>	__( 'This is a read-only value.', 'admin-page-framework-demo' ),
-                'description'	=>	__( 'The attribute can be set with the <code>attributes</code> key.', 'admin-page-framework-demo' ),
+                'value'	=>	'Placeholder text',
+                'description'	=>	'Original Name'
             ),
             array(	// Multiple text fields
-                'field_id'	=>	'text_multiple',
-                'title'	=>	__( 'Multiple Text Fields', 'admin-page-framework-demo' ),
-                'help'	=>	__( 'Multiple text fields can be passed by setting an array to the label key.', 'admin-page-framework-demo' ),
+                'field_id'	=>	'modify_host',
+                'title'	=>	'Host Name',
+                'help'	=>	'Modify the host name.',
                 'type'	=>	'text',
-                'default'	=>	'Hello World',
-                'label'	=>	'First Item: ',
+                'default'	=>	'Placeholder text',
+                'label'	=>	'Original Host:',
                 'attributes'	=>	array(
-                    'size'	=>	20,
+                    'value'	=>	'modify old host',
+                    'readonly'	=>	'ReadOnly',
                 ),
                 'delimiter'	=>	'<br />',
                 array(
-                    'default'	=>	'Foo bar',
-                    'label'	=>	'Second Item: ',
+                    'default'	=>	'Placeholder new host',
+                    'label'	=>	'New Host: ',
                     'attributes'	=>	array(
-                        'size'	=>	40,
+                        'field_id' => 'modify_host',
+                        'readonly'	=>	false,
                     )
                 ),
-                array(
-                    'default'	=>	'Yes, we can',
-                    'label'	=>	'Third Item: ',
-                    'attributes'	=>	array(
-                        'size'	=>	60,
-                    )
-                ),
-                'description'	=>	__( 'These are multiple text fields. To include multiple input fields associated with one field ID, use the numeric keys in the field definition array.', 'admin-page-framework-demo' ),
+                //'description'	=>	'Modify the host.'
             ),
             array( // Submit button
                 'field_id' => 'submit_button_b',
@@ -95,10 +77,10 @@ class Manage_Events extends AdminPageFramework {
 
     }
 
-    public function start_Manage_Events() {
+    /*public function start_Manage_Events() {
 
         $this->setFooterInfoLeft( '<br />Smelter Text on the left hand side.', false );
-    }
+    }*/
 
     public function content_foot_manage_events_page( $sContent ) {
 
@@ -156,7 +138,7 @@ class Manage_Events extends AdminPageFramework {
     // So the slug should not contain characters which cannot be used in function names such as dots and hyphens.
     public function do_manage_events_page() {
 
-        $this->setFooterInfoLeft( '<br />Orange Text on the left hand side.', false );
+        /* $this->setFooterInfoLeft( '<br />Orange Text on the left hand side.', false );*/
 
         ?>
 
