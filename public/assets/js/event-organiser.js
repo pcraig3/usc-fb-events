0c015b86657a8a71d05602643de92611c027a6de
Paul Craig <pcraig3@uwo.ca>
2014-09-18 16:02:31 -0400

    Event calendar loading symbol is now a horse.

--- public/assets/js/event-organiser.js
+++ public/assets/js/event-organiser.js
@@ -5,12 +5,59 @@
 
         // JavaScript here will only be included on pages with an Event-Organiser fullcalendar
 
-        $('.eo-fullcalendar')
+        var $eo_fullcalendar = $('.eo-fullcalendar');
+
+        $eo_fullcalendar
             .on( 'click', '.fc-event', function(e){
                 e.preventDefault();
                 window.open( $(this).attr('href'), '_blank' );
-            });
+        });
+
+        loading_horse( $eo_fullcalendar );
 
     });
+
+    function loading_horse( $eo_fullcalendar ) {
+
+        var loading = document.getElementById('eo_fullcalendar_1_loading');
+        var $loading = $( loading );
+
+        var loading_img = loading.firstChild;
+        loading_img.src = 'http://westernusc.org/wp-content/plugins/usc-fb-events/assets/horse.gif';
+
+        var loading_gif_styling = {
+            //position: absolute; //this is the default
+            //z-index: 5;  //this is the default
+            //display: none
+            //background-color: white
+
+            textAlign:      'center',
+            width:          '100%',
+            boxSizing:      'border-box',
+            borderTop:      '1px dashed #ddd',
+            borderBottom:   '1px dashed #ddd',
+            visibility:     'visible',
+            backgroundColor:'rgba(255,255,255, .8)',
+            height:         51 + 'px'
+        };
+
+        //get rid of anything after the image (ie, the 'Loading...' phrase)
+        loading.innerHTML = loading.innerHTML.substring(0, loading.innerHTML.indexOf('>') + 1);
+
+        $loading.css( loading_gif_styling );
+
+        //basically, click on any of the buttons and get a new height
+        $eo_fullcalendar
+            .on( 'click', '.fc-button', function(){
+                //set a new height for the loading div
+                console.log('CLICK!');
+                var height_sticky_title = $('#usc_fb_events_fullcalendar__list__header').height();
+
+                var height_header = $eo_fullcalendar.find('.fc-header').height();
+
+                $loading.css('height', height_header + height_sticky_title + 'px');
+        });
+
+    }
 
 }(jQuery));
